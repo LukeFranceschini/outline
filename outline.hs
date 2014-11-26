40c58ece7365c3555ae2a0e5272314ad06cb3782
l3france <luke.a.franceschini@gmail.com>
2014-11-25 23:14:44 -0500

    Change the file format to use read for the types, which are instances of the Read typeclass. Points are now implemented using arrays, because Points should be able to have any number of dimensions.

--- outline.hs
+++ outline.hs
@@ -1,7 +1,14 @@
-data Point t = Point t t t
+data Point t = Point [t] deriving (Show, Read)
 
-type Orientation t = Point t
+type FocalPoint = Point
 
-data Camera t = Camera (Point t) (Orientation t)
+data Camera t = Camera (Point t) (FocalPoint t) deriving (Show, Read)
 
-data Edge t = Edge (Point t) (Point t)
+data Edge t = Edge (Point t) (Point t) deriving (Show, Read)
+
+data Pixel = Black | White deriving (Show, Read)
+
+data Row = [pixel] deriving (Show, Read)
+
+data Screen = Screen [Row] deriving (Show, Read)
+
