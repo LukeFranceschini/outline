73f9ea101628c4e0234e23d53bfa00260036250e
l3france <luke.a.franceschini@gmail.com>
2014-11-27 23:46:18 -0500

    Forgot to stage files before committing.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A wireframe renderer written in haskell.
 
 This purpose of this project is to be able to create wireframe images in black
 and white, from the perspective of a camera which can be at any position, 
-facing any direction. A wireframe has been chosen because it is relatively, 
+facing any direction. A wireframe has been chosen because it is relatively easy,
 with no need to worry about occlusion culling or lighting surfaces.
 
 A simple file format has been created for this project, detailed in 
--- outline.hs
+++ outline.hs
@@ -1,14 +1,22 @@
+import Control.Monad
+
 data Point t = Point [t] deriving (Show, Read)
 
-type FocalPoint = Point
-
-data Camera t = Camera (Point t) (FocalPoint t) deriving (Show, Read)
+data Camera t = Camera {location :: Point t, orientation :: Point t} deriving (Show, Read)
 
 data Edge t = Edge (Point t) (Point t) deriving (Show, Read)
 
 data Pixel = Black | White deriving (Show, Read)
 
-data Row = [pixel] deriving (Show, Read)
+data Row = Row [Pixel] deriving (Show, Read)
 
 data Screen = Screen [Row] deriving (Show, Read)
 
+main = do
+    camStr <- getLine
+    edgeStr <- getLine
+    let cam = read camStr :: Camera Int
+        edges = read edgeStr :: [Edge Int]
+    putStrLn $ "The camera is at " ++ (show (location cam)) ++ " facing " ++ (show $ orientation cam) ++ "."
+    putStrLn $ "This program will eventually be able to determine which of the following edges the camera can see and then draw them:"
+    forM edges $ putStrLn . show
