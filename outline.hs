82c4dbc5f2fbf158a6ac34f7e9c82d7998b47515
l3france <luke.a.franceschini@gmail.com>
2014-12-08 22:27:22 -0500

    Read input file as argument. Add subpoint method.
    The file which will be used to generate the wireframe is now read as a
    command line argument, instead of simply being piped to stdin.
    Additionally, this requires the small change of the program reading from
    a file instead of standard in, so we use withFile to get the camera and
    edge list from the file.
    
    To render the wireframe, we are going to translate our entire scene so
    that the camera is located at (0, 0, 0). This should hopefully make some
    of the math easier later on because the orientation will now be a vector
    which is the direction the camera is facing. This makes it easier to
    locate a plane perpendicular to this direction (more on that in future
    commits) and project onto it since it will pass through the origin.
    To translate these points, we need a way to subtract two points, which
    is where the subPoint method comes in. We will use it later to subtract
    the camera's location from all points, which is how we center the camera
    at the origin.

--- outline.hs
+++ outline.hs
@@ -1,4 +1,6 @@
 import Control.Monad
+import System.Environment
+import System.IO
 
 data Point t = Point [t] deriving (Show, Read)
 
@@ -12,9 +14,22 @@ data Row = Row [Pixel] deriving (Show, Read)
 
 data Screen = Screen [Row] deriving (Show, Read)
 
+subPoint :: (Num t) => Point t -> Point t -> Either String (Point t)
+subPoint (Point []) (Point []) = Right (Point [])
+subPoint (Point []) _ = Left "Point1 has fewer dimensions than Point2"
+subPoint _ (Point []) = Left "Point2 has fewer dimensions than Point1"
+subPoint (Point (a : arest)) (Point (b : brest)) = 
+    let rest = (Point arest) `subPoint` (Point brest)
+    in case rest of
+        Left error -> Left error
+        Right (Point otherDimensions) -> Right (Point ((a - b) : otherDimensions))
+
 main = do
-    camStr <- getLine
-    edgeStr <- getLine
+    (file:otherArgs) <- getArgs
+    (camStr, edgeStr) <- withFile file ReadMode (\handle -> do
+                            camStr <- hGetLine handle
+                            edgeStr <- hGetLine handle
+                            return (camStr, edgeStr))
     let cam = read camStr :: Camera Int
         edges = read edgeStr :: [Edge Int]
     putStrLn $ "The camera is at " ++ (show (location cam)) ++ " facing " ++ (show $ orientation cam) ++ "."
